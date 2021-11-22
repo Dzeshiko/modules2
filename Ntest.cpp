73cc15ff53ea12366d974bd3aad2d237abe27ff3
Dzeshiko <76652787+Dzeshiko@users.noreply.github.com>
2021-11-22 14:37:15 +0200

    Оновити main.blocks, main.ts, Ntest.cpp

--- Ntest.cpp
+++ Ntest.cpp
@@ -5,9 +5,10 @@ namespace hello
  /**
      * test comments
      */
-     //% block="real_name_of_block"
-//% blockId=do_something
+    //% block="real_name_of_block"
+    //% blockId=do_something
     void do_something()
     {
+        uBit.display.scroll("HELLO!");
     }
 }
